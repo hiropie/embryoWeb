a795f454c93482dbae30c3acc37dcc68c0b0873f
hiropie <hi610robo@outlook.jp>
2021-10-16 00:53:49 +0900

    20211015

---
+++ models/chat.js
@@ -0,0 +1,53 @@
+const socketio = require('socket.io');
+const fs = require('fs');
+const modify = require('./../public/javascripts/modify');
+const generator = require('./../public/javascripts/gif');
+const boardDo = require('./../public/javascripts/arduino');
+const { Led } = require('johnny-five');
+
+var accessNum = 1;
+let count = 1;
+const WIDTH   = 1280;             // 画像サイズ X
+const HEIGHT  = 720;             // 画像サイズ Y
+const GifFile = './public/images/gifs/actionScreen';   // 出力ファイル名
+
+function chat(server) {
+  boardDo;
+    const sio = socketio.listen(server);
+    sio.on('connection', function(socket) {
+        socket.on('chat-message', function() {
+            accessNum++;
+            console.log('Send message to client accessNum: '+accessNum);
+            generator.capture(modify.toImage+modify.imgPath).then(()=>{
+                new Promise((resolve)=>{
+                  setTimeout(() => {
+                    resolve();
+                  }, 2000);
+                  console.log("2秒経過");
+                }).then(()=>{
+                  generator.mkGif(accessNum);
+                }).then(()=>{
+                  sio.emit('chat-message', accessNum);
+                })
+              });
+        });
+        setInterval(()=>{
+          console.log("  気温 : ", tmp);
+          console.log("  湿度 : ", hum);
+          socket.emit('humi', hum);
+          socket.emit('temp', tmp);
+        },5000);
+
+        socket.on('humUp',function(){
+          led.on();
+          setTimeout(()=>{
+            led.off();
+          },2000);
+        })
+        socket.on("disconnect", function() {
+
+        });
+    });
+};
+
+module.exports = chat;
