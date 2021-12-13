e4402d6408b680f33d8d978359784f7803ef20f0
hiropie <hi610robo.outlook.jp>
2021-12-13 17:19:14 +0900

    tyousei

--- public/javascripts/web-socket.js
+++ public/javascripts/web-socket.js
@@ -1,3 +1,4 @@
+
 let dataT = [];
 let dataH = [];
 let timeList = [];
@@ -22,9 +23,9 @@ let optionsT = {
         style: {
             fontSize: '24px',
         },
-    　　title: {
-                text: '時刻',         
-            },
+        title: {
+            text: '時刻',
+        },
         categories: [],
         // type: "datetime",
         tickInterval: 360,
@@ -68,8 +69,8 @@ let optionsH = {
         style: {
             fontSize: '24px',
         },
-　　    title: {
-            text: '時刻',         
+        title: {
+            text: '時刻',
         },
         categories: [],
         // type: "datetime",
@@ -108,65 +109,65 @@ optionsT.xAxis[0].categories = timeList;
 optionsT.series[0].data = timeTmp;
 chart1 = new Highcharts.Chart('tmpGraph', optionsT);
 
-$(function() {
+$(function () {
     const socket = io();
-    $('#reload').click(function() {
+    $('#reload').click(function () {
         socket.emit('capture', $('#reload').val());
         $('#reload').val('');
         return false;
     });
 
-    $('#change').click(function(){
-        socket.emit('draw',$('#change').val());
+    $('#change').click(function () {
+        socket.emit('draw', $('#change').val());
         socket.emit('update');
     });
 
-    $('#push').click(function(){
+    $('#push').click(function () {
         socket.emit('humUp', $('#push').val());
         return false;
     });
-    
-    setTimeout(function update(){
-        socket.emit("draw")
-        setTimeout(update,60000);
-    },60000)
 
-    socket.on("connect",function(start){
+    setTimeout(function update() {
+        socket.emit("draw")
+        setTimeout(update, 60000);
+    }, 60000)
+
+    socket.on("connect", function (start) {
         const startTime = start;
     })
-    
-    socket.on('Show', function(count) {
+
+    socket.on('Show', function (count) {
         $('#num').text(count);
         $('#embryo_gif').attr('src', '/images/basis/first.png');
-        setTimeout(function(){
-            $('#embryo_gif').attr('src', '/images/gifs/actionScreen'+(count)+'.gif');
-        },500)        
+        setTimeout(function () {
+            $('#embryo_gif').attr('src', '/images/gifs/actionScreen' + (count) + '.gif');
+        }, 500)
     });
 
-    socket.on('temp', function(tmp){
+    socket.on('temp', function (tmp) {
         timeTmp = tmp;
     });
 
-    socket.on('humi', function(hum){
+    socket.on('humi', function (hum) {
         timeHum = hum;
     });
 
 
-    socket.on('nowTime', function(time){
-        for(let i=0; i<time.length; i++){ 
+    socket.on('nowTime', function (time) {
+        for (let i = 0; i < time.length; i++) {
             clock = toHour(time[i])
-            timeList[i] = clock.hour + ":" + clock.min + ":" + clock.sec ;
+            timeList[i] = clock.hour + ":" + clock.min + ":" + clock.sec;
         }
         // now = toHour(Math.round(timeList[timeList.length - 1]));        
         $('#time').text(now.hour + ":" + now.min + ":" + now.sec);
-        $('#tmp').text(Math.round(timeTmp[timeTmp.length-1]));
-        $('#hum').text(Math.round(timeHum[timeHum.length-1]));
+        $('#tmp').text(Math.round(timeTmp[timeTmp.length - 1]));
+        $('#hum').text(Math.round(timeHum[timeHum.length - 1]));
         // ('#time').text(timeList.length);
         // $('#tmp').text(timeTmp.length);
         // $('#hum').text(timeHum.length);
     });
 
-    socket.on("reDraw",function(){
+    socket.on("reDraw", function () {
         optionsH.xAxis[0].categories = timeList;
         optionsH.series[0].data = timeHum;
         chart2 = new Highcharts.Chart("humGraph", optionsH);
@@ -177,13 +178,13 @@ $(function() {
     });
 });
 
-function toHour(time){
+function toHour(time) {
     let sec = (time % 60) % 60;
     let min = Math.floor(time / 60) % 60;
     let hour = Math.floor(time / 3600);
-    
-    return{
-        hour : hour,
+
+    return {
+        hour: hour,
         min: min,
         sec: sec
     }
